f79cd03788c0756f653bc872c2a842d57c8f8803
Penny0922 <727449661@qq.com>
2021-12-23 16:13:36 +0800

    add finereport

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,1 +1,13 @@
-<template>权限</template>
+<template>
+  <div class="app-container">
+    <iframe
+      id="reportFrame"
+      width="100%"
+      height="600"
+      frameborder="0"
+      scrolling="auto"
+      src="http://localhost:8075/webroot/decision/view/report?viewlet=ordersale.cpt"
+    >
+    </iframe>
+  </div>
+</template>
